0806d01f519c0bd329c9de0dc54fdb4c7a2ace00
Cgarg547 <chiraggargkkp@gmail.com>
2024-10-31 02:17:46 -0400

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,330 +1,117 @@
 import React, { useEffect, useState } from "react";
-import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import {
-  LoginPage,
-  SignupPage,
-  ActivationPage,
-  HomePage,
-  ProductsPage,
-  BestSellingPage,
-  EventsPage,
-  FAQPage,
-  CheckoutPage,
-  PaymentPage,
+import { ToastContainer } from "react-toastify";
+import axios from "axios";
+import { Elements } from "@stripe/react-stripe-js";
+import { loadStripe } from "@stripe/stripe-js";
+import Store from "./redux/store";
+import { loadUser, loadSeller } from "./redux/actions/user";
+import { getAllProducts } from "./redux/actions/product";
+import { getAllEvents } from "./redux/actions/event";
+import ProtectedRoute from "./routes/ProtectedRoute";
+import ProtectedAdminRoute from "./routes/ProtectedAdminRoute";
+import SellerProtectedRoute from "./routes/SellerProtectedRoute";
+import { server } from "./server";
+import "./App.css";
+import "react-toastify/dist/ReactToastify.css";
+import { 
+  HomePage, 
+  LoginPage, 
+  SignupPage, 
+  ActivationPage, 
+  ProductsPage, 
+  ProductDetailsPage, 
+  CheckoutPage, 
+  PaymentPage, 
   OrderSuccessPage,
-  ProductDetailsPage,
   ProfilePage,
-  ShopCreatePage,
-  SellerActivationPage,
-  ShopLoginPage,
-  OrderDetailsPage,
-  TrackOrderPage,
   UserInbox,
-} from "./routes/Routes.js";
-import {
+  TrackOrderPage,
+  ShopHomePage,
+  ShopCreatePage,
+  ShopLoginPage,
   ShopDashboardPage,
-  ShopCreateProduct,
-  ShopAllProducts,
-  ShopCreateEvents,
-  ShopAllEvents,
-  ShopAllCoupouns,
-  ShopPreviewPage,
-  ShopAllOrders,
-  ShopOrderDetails,
-  ShopAllRefunds,
   ShopSettingsPage,
-  ShopWithDrawMoneyPage,
-  ShopInboxPage,
-} from "./routes/ShopRoutes";
-import {
   AdminDashboardPage,
   AdminDashboardUsers,
   AdminDashboardSellers,
   AdminDashboardOrders,
   AdminDashboardProducts,
   AdminDashboardEvents,
-  AdminDashboardWithdraw
-} from "./routes/AdminRoutes";
-import { ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
-import Store from "./redux/store";
-import { loadSeller, loadUser } from "./redux/actions/user";
-import ProtectedRoute from "./routes/ProtectedRoute";
-import ProtectedAdminRoute from "./routes/ProtectedAdminRoute";
-import { ShopHomePage } from "./ShopRoutes.js";
-import SellerProtectedRoute from "./routes/SellerProtectedRoute";
-import { getAllProducts } from "./redux/actions/product";
-import { getAllEvents } from "./redux/actions/event";
-import axios from "axios";
-import { server } from "./server";
-import { Elements } from "@stripe/react-stripe-js";
-import { loadStripe } from "@stripe/stripe-js";
+  AdminDashboardWithdraw,
+  // Import other components as necessary
+} from "./routes/Routes";
 
 const App = () => {
-  const [stripeApikey, setStripeApiKey] = useState("");
+  const [stripeApiKey, setStripeApiKey] = useState("");
 
-  async function getStripeApikey() {
-    const { data } = await axios.get(`${server}/payment/stripeapikey`);
-    setStripeApiKey(data.stripeApikey);
-  }
+  // Function to fetch Stripe API key
+  const fetchStripeApiKey = async () => {
+    try {
+      const response = await axios.get(`${server}/payment/stripeapikey`);
+      setStripeApiKey(response.data.stripeApikey);
+    } catch (error) {
+      console.error("Error fetching Stripe API key:", error);
+    }
+  };
+
+  // Use effect to load necessary data when the app starts
   useEffect(() => {
     Store.dispatch(loadUser());
     Store.dispatch(loadSeller());
     Store.dispatch(getAllProducts());
     Store.dispatch(getAllEvents());
-    getStripeApikey();
+    fetchStripeApiKey();
   }, []);
 
   return (
     <BrowserRouter>
-      {stripeApikey && (
-        <Elements stripe={loadStripe(stripeApikey)}>
+      {/* Stripe Elements for payment */}
+      {stripeApiKey && (
+        <Elements stripe={loadStripe(stripeApiKey)}>
           <Routes>
-            <Route
-              path="/payment"
-              element={
-                <ProtectedRoute>
-                  <PaymentPage />
-                </ProtectedRoute>
-              }
-            />
+            <Route path="/payment" element={<ProtectedRoute><PaymentPage /></ProtectedRoute>} />
           </Routes>
         </Elements>
       )}
+
+      {/* Main application routes */}
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/sign-up" element={<SignupPage />} />
-        <Route
-          path="/activation/:activation_token"
-          element={<ActivationPage />}
-        />
-        <Route
-          path="/seller/activation/:activation_token"
-          element={<SellerActivationPage />}
-        />
+        <Route path="/activation/:activation_token" element={<ActivationPage />} />
         <Route path="/products" element={<ProductsPage />} />
-
         <Route path="/product/:id" element={<ProductDetailsPage />} />
-        <Route path="/best-selling" element={<BestSellingPage />} />
-        <Route path="/events" element={<EventsPage />} />
-        <Route path="/faq" element={<FAQPage />} />
-        <Route
-          path="/checkout"
-          element={
-            <ProtectedRoute>
-              <CheckoutPage />
-            </ProtectedRoute>
-          }
-        />
+        <Route path="/checkout" element={<ProtectedRoute><CheckoutPage /></ProtectedRoute>} />
         <Route path="/order/success" element={<OrderSuccessPage />} />
-        <Route
-          path="/profile"
-          element={
-            <ProtectedRoute>
-              <ProfilePage />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/inbox"
-          element={
-            <ProtectedRoute>
-              <UserInbox />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/user/order/:id"
-          element={
-            <ProtectedRoute>
-              <OrderDetailsPage />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/user/track/order/:id"
-          element={
-            <ProtectedRoute>
-              <TrackOrderPage />
-            </ProtectedRoute>
-          }
-        />
-        <Route path="/shop/preview/:id" element={<ShopPreviewPage />} />
-        {/* shop Routes */}
+        <Route path="/profile" element={<ProtectedRoute><ProfilePage /></ProtectedRoute>} />
+        <Route path="/inbox" element={<ProtectedRoute><UserInbox /></ProtectedRoute>} />
+        <Route path="/user/track/order/:id" element={<ProtectedRoute><TrackOrderPage /></ProtectedRoute>} />
+
+        {/* Seller routes */}
         <Route path="/shop-create" element={<ShopCreatePage />} />
         <Route path="/shop-login" element={<ShopLoginPage />} />
-        <Route
-          path="/shop/:id"
-          element={
-            <SellerProtectedRoute>
-              <ShopHomePage />
-            </SellerProtectedRoute>
-          }
-        />
-        <Route
-          path="/settings"
-          element={
-            <SellerProtectedRoute>
-              <ShopSettingsPage />
-            </SellerProtectedRoute>
-          }
-        />
-        <Route
-          path="/dashboard"
-          element={
-            <SellerProtectedRoute>
-              <ShopDashboardPage />
-            </SellerProtectedRoute>
-          }
-        />
-        <Route
-          path="/dashboard-create-product"
-          element={
-            <SellerProtectedRoute>
-              <ShopCreateProduct />
-            </SellerProtectedRoute>
-          }
-        />
-        <Route
-          path="/dashboard-orders"
-          element={
-            <SellerProtectedRoute>
-              <ShopAllOrders />
-            </SellerProtectedRoute>
-          }
-        />
-        <Route
-          path="/dashboard-refunds"
-          element={
-            <SellerProtectedRoute>
-              <ShopAllRefunds />
-            </SellerProtectedRoute>
-          }
-        />
-
-        <Route
-          path="/order/:id"
-          element={
-            <SellerProtectedRoute>
-              <ShopOrderDetails />
-            </SellerProtectedRoute>
-          }
-        />
-        <Route
-          path="/dashboard-products"
-          element={
-            <SellerProtectedRoute>
-              <ShopAllProducts />
-            </SellerProtectedRoute>
-          }
-        />
-        <Route
-          path="/dashboard-create-event"
-          element={
-            <SellerProtectedRoute>
-              <ShopCreateEvents />
-            </SellerProtectedRoute>
-          }
-        />
-        <Route
-          path="/dashboard-events"
-          element={
-            <SellerProtectedRoute>
-              <ShopAllEvents />
-            </SellerProtectedRoute>
-          }
-        />
-        <Route
-          path="/dashboard-coupouns"
-          element={
-            <SellerProtectedRoute>
-              <ShopAllCoupouns />
-            </SellerProtectedRoute>
-          }
-        />
-        <Route
-          path="/dashboard-withdraw-money"
-          element={
-            <SellerProtectedRoute>
-              <ShopWithDrawMoneyPage />
-            </SellerProtectedRoute>
-          }
-        />
-        <Route
-          path="/dashboard-messages"
-          element={
-            <SellerProtectedRoute>
-              <ShopInboxPage />
-            </SellerProtectedRoute>
-          }
-        />
-        {/* Admin Routes */}
-        <Route
-          path="/admin/dashboard"
-          element={
-            <ProtectedAdminRoute>
-              <AdminDashboardPage />
-            </ProtectedAdminRoute>
-          }
-        />
-        <Route
-          path="/admin-users"
-          element={
-            <ProtectedAdminRoute>
-              <AdminDashboardUsers />
-            </ProtectedAdminRoute>
-          }
-        />
-        <Route
-          path="/admin-sellers"
-          element={
-            <ProtectedAdminRoute>
-              <AdminDashboardSellers />
-            </ProtectedAdminRoute>
-          }
-        />
-        <Route
-          path="/admin-orders"
-          element={
-            <ProtectedAdminRoute>
-              <AdminDashboardOrders />
-            </ProtectedAdminRoute>
-          }
-        />
-        <Route
-          path="/admin-products"
-          element={
-            <ProtectedAdminRoute>
-              <AdminDashboardProducts />
-            </ProtectedAdminRoute>
-          }
-        />
-        <Route
-          path="/admin-events"
-          element={
-            <ProtectedAdminRoute>
-              <AdminDashboardEvents />
-            </ProtectedAdminRoute>
-          }
-        />
-        <Route
-          path="/admin-withdraw-request"
-          element={
-            <ProtectedAdminRoute>
-              <AdminDashboardWithdraw />
-            </ProtectedAdminRoute>
-          }
-        />
+        <Route path="/shop/:id" element={<SellerProtectedRoute><ShopHomePage /></SellerProtectedRoute>} />
+        <Route path="/settings" element={<SellerProtectedRoute><ShopSettingsPage /></SellerProtectedRoute>} />
+        <Route path="/dashboard" element={<SellerProtectedRoute><ShopDashboardPage /></SellerProtectedRoute>} />
+        
+        {/* Admin routes */}
+        <Route path="/admin/dashboard" element={<ProtectedAdminRoute><AdminDashboardPage /></ProtectedAdminRoute>} />
+        <Route path="/admin-users" element={<ProtectedAdminRoute><AdminDashboardUsers /></ProtectedAdminRoute>} />
+        <Route path="/admin-sellers" element={<ProtectedAdminRoute><AdminDashboardSellers /></ProtectedAdminRoute>} />
+        <Route path="/admin-orders" element={<ProtectedAdminRoute><AdminDashboardOrders /></ProtectedAdminRoute>} />
+        <Route path="/admin-products" element={<ProtectedAdminRoute><AdminDashboardProducts /></ProtectedAdminRoute>} />
+        <Route path="/admin-events" element={<ProtectedAdminRoute><AdminDashboardEvents /></ProtectedAdminRoute>} />
+        <Route path="/admin-withdraw-request" element={<ProtectedAdminRoute><AdminDashboardWithdraw /></ProtectedAdminRoute>} />
       </Routes>
+
+      {/* Toast notifications */}
       <ToastContainer
         position="bottom-center"
         autoClose={5000}
         hideProgressBar={false}
-        newestOnTop={false}
         closeOnClick
-        rtl={false}
         pauseOnFocusLoss
         draggable
         pauseOnHover
